04718f898da99023d95c9456abd6861e72516408
DamonNG <DamonNG@DamonNGdeMacBook-Air.local>
2015-07-03 14:41:04 +0800

    Initial Commit

---
+++ ForFlipCountDownDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ForFlipCountDownDemo
+//
+//  Created by DamonNG on 15/7/3.
+//  Copyright (c) 2015年 JiehaoWu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ForFlipCountDownDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ForFlipCountDownDemo
+//
+//  Created by DamonNG on 15/7/3.
+//  Copyright (c) 2015年 JiehaoWu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
